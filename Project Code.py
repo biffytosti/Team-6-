9cef735f0149a376e933988cabc5ec9cc340498b
Trager Conard <97655957+tragerconard@users.noreply.github.com>
2022-04-13 14:24:19 -0500

    Add files via upload

--- Project Code.py
+++ Project Code.py
@@ -23,35 +23,50 @@ class OrderCostCalculator:
         Label(window, text = "Select Color").grid(row = 3, column = 1, sticky = W)
         Label(window, text = "Select Size").grid(row = 4, column = 1, sticky = W)
         Label(window, text = "Select Quantity").grid(row = 5, column = 1, sticky = W)
-        Label(window, text = "Loan Amount").grid(row = 6, column = 1, sticky = W)
-        Label(window, text = "Monthly Payment").grid(row = 7, column = 1, sticky = W)
-        Label(window, text = "Total Payment").grid(row = 8, column = 1, sticky = W)
+        Label(window, text = "Subtotal Cost").grid(row = 6, column = 1, sticky = W)
+        Label(window, text = "Shipping and Tax").grid(row = 7, column = 1, sticky = W)
+        Label(window, text = "$5.00 + 8.25% Sales Tax in Texas").grid(row = 7, column = 7, sticky = W)
+        Label(window, text = "Compute Total Cost").grid(row = 8, column = 7, sticky = W)
         
-        #For each of the three inputs, create string variable and get their values through Entry control
-        self.annualInterestRateVar = StringVar()
-        Entry(window, textvariable = self.annualInterestRateVar, justify = RIGHT).grid(row = 2, column = 2)
-        self.numberOfYearsVar = StringVar()
-        Entry(window, textvariable = self.numberOfYearsVar, justify = RIGHT).grid(row = 3, column = 2)
-        self.loanAmountVar = StringVar()
-        Entry(window, textvariable = self.loanAmountVar, justify = RIGHT).grid(row = 4, column = 2)
+        #For each of the three inputs, we created radio buttons and check buttons to select which product(s), color(s), and size(s)
+        self.ProductVar = IntVar()
+        Radiobutton(window, text = 'Short Sleeve: $22', variable = self.ProductVar, value = 'ShortSleeve', justify = CENTER).grid(row = 2, column = 2)
+        Radiobutton(window, text = 'Long Sleeve: $30', variable = self.ProductVar, value = 'LongSleeve', justify = CENTER).grid(row = 2, column = 3)
+        Radiobutton(window, text = 'Hoodie: $45', variable = self.ProductVar, value = 'Hoodie', justify = CENTER).grid(row = 2, column = 4)
+        Radiobutton(window, text = 'Sweat Pants: $40', variable = self.ProductVar, value = 'SweatPants', justify = CENTER).grid(row = 2, column = 5)
+        self.ColorVar = IntVar()
+        Radiobutton(window, text = 'Black', variable = self.ColorVar, value = 'Black',  justify = CENTER).grid(row = 3, column = 2)
+        Radiobutton(window, text = 'White', variable = self.ColorVar, value = 'White', justify = CENTER).grid(row = 3, column = 3)
+        Radiobutton(window, text = 'Purple', variable = self.ColorVar, value = 'Purple', justify = CENTER).grid(row = 3, column = 4)
+        Radiobutton(window, text = 'Gray', variable = self.ColorVar, value = 'Gray', justify = CENTER).grid(row = 3, column = 5)
+        self.Size = IntVar()
+        Radiobutton(window, text = 'XS', variable = self.Size, value = 'XS', justify = CENTER).grid(row = 4, column = 2)
+        Radiobutton(window, text = 'S', variable = self.Size, value = 'S', justify = CENTER).grid(row = 4, column = 3)
+        Radiobutton(window, text = 'M', variable = self.Size, value = 'M', justify = CENTER).grid(row = 4, column = 4)
+        Radiobutton(window, text = 'L', variable = self.Size, value = 'L', justify = CENTER).grid(row = 4, column = 5)
+        Radiobutton(window, text = 'XL', variable = self.Size, value = 'XL', justify = CENTER).grid(row = 4, column = 6)
+        self.Quantity = StringVar()
+        Entry(window, textvariable = self.Quantity, justify = CENTER).grid(row = 5, column = 7)
+        self.Subtotal = StringVar()
+        Entry(window, textvariable = self.Subtotal, justify = CENTER).grid(row = 6, column = 7)
         
-        #For each of the two outputs, crete a String variable and output each to a Label in the Window
+        #For each of the two outputs, create a String variable and output each to a Label in the Window
         self.monthlyPaymentVar = StringVar()
         Label(window, textvariable = self.monthlyPaymentVar).grid(row = 5, column = 2, sticky = E)
         self.totalPaymentVar = StringVar()
         Label(window, textvariable = self.totalPaymentVar).grid(row = 6, column = 2, sticky = E)
         
-        #Create a Compute Payment button to call the computePayment function
-        Button(window, text = "Compute Payment", command = self.computePayment).grid(row = 7, column = 2, sticky = E)
+        #Create a Compute Total Cost button to call the computeTotalCost function
+        Button(window, text = "Compute Total Cost", command = self.computeTotalCost).grid(row = 8, column = 7, sticky = E)
         
         window.mainloop() # Create an event loop
         
-    def getMonthlyPayment(self,loanAmount, monthlyInterestRate, numberOfYears):
+    def getMonthlyPayment(self, Subtotal, SalesTax, numberOfYears):
         #this function calculates a monthly payment based on the user data entered and returns the value of the monthly payment
         monthlyPayment = loanAmount * monthlyInterestRate / (1 - 1 / (1 + monthlyInterestRate) ** (numberOfYears * 12))
         return monthlyPayment
 
-    def computePayment(self):
+    def computeTotalCost(self):
         #this function calls getMonthlyPayment function with the input values entered into the textboxed (Entry controls) and assigned to corresponding variables, 
         # after converting them to numerical values; after the monthly payment is returned, it is assigned to a variable for output in the window and 
         #total payment is calculated and assigned to a variable to output in the window
