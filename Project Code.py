6f1780947429b0bc72a3a04bd1f86fb2d66ece56
Trager Conard <97655957+tragerconard@users.noreply.github.com>
2022-04-13 17:11:11 -0500

    Add files via upload

--- Project Code.py
+++ Project Code.py
@@ -25,8 +25,7 @@ class OrderCostCalculator:
         Label(window, text = "Select Quantity").grid(row = 5, column = 1, sticky = W)
         Label(window, text = "Subtotal Cost").grid(row = 6, column = 1, sticky = W)
         Label(window, text = "Shipping and Tax").grid(row = 7, column = 1, sticky = W)
-        Label(window, text = "$5.00 + 8.25% Sales Tax in Texas").grid(row = 7, column = 7, sticky = W)
-        Label(window, text = "Compute Total Cost").grid(row = 8, column = 7, sticky = W)
+        Label(window, text = "Total Cost").grid(row = 8, column = 1, sticky = W)
         
         #For each of the three inputs, we created radio buttons and check buttons to select which product(s), color(s), and size(s)
         self.ProductVar = IntVar()
@@ -45,40 +44,37 @@ class OrderCostCalculator:
         Radiobutton(window, text = 'M', variable = self.Size, value = 'M', justify = CENTER).grid(row = 4, column = 4)
         Radiobutton(window, text = 'L', variable = self.Size, value = 'L', justify = CENTER).grid(row = 4, column = 5)
         Radiobutton(window, text = 'XL', variable = self.Size, value = 'XL', justify = CENTER).grid(row = 4, column = 6)
-        self.Quantity = StringVar()
+        self.Quantity = IntVar()
         Entry(window, textvariable = self.Quantity, justify = CENTER).grid(row = 5, column = 7)
-        self.Subtotal = StringVar()
+        self.Subtotal = IntVar()
         Entry(window, textvariable = self.Subtotal, justify = CENTER).grid(row = 6, column = 7)
         
         #For each of the two outputs, create a String variable and output each to a Label in the Window
-        self.monthlyPaymentVar = StringVar()
-        Label(window, textvariable = self.monthlyPaymentVar).grid(row = 5, column = 2, sticky = E)
-        self.totalPaymentVar = StringVar()
-        Label(window, textvariable = self.totalPaymentVar).grid(row = 6, column = 2, sticky = E)
+        self.TaxAndShippingVar = StringVar()
+        Label(window, textvariable = self.TaxAndShippingVar).grid(row = 7, column = 7, sticky = E)
+        self.FinalPrice = StringVar()
+        Label(window, textvariable = self.FinalPrice).grid(row = 8, column = 7, sticky = E)
         
         #Create a Compute Total Cost button to call the computeTotalCost function
-        Button(window, text = "Compute Total Cost", command = self.computeTotalCost).grid(row = 8, column = 7, sticky = E)
+        Button(window, text = "Compute Total Cost", command = self.computeTotalCost).grid(row = 9, column = 7, sticky = E)
         
         window.mainloop() # Create an event loop
         
-    def getMonthlyPayment(self, Subtotal, SalesTax, numberOfYears):
+    def getTaxAndShipping(self, Subtotal):
         #this function calculates a monthly payment based on the user data entered and returns the value of the monthly payment
-        monthlyPayment = loanAmount * monthlyInterestRate / (1 - 1 / (1 + monthlyInterestRate) ** (numberOfYears * 12))
-        return monthlyPayment
+        TaxAndShipping = (Subtotal * 0.0825) + 5
+        return TaxAndShipping
 
     def computeTotalCost(self):
         #this function calls getMonthlyPayment function with the input values entered into the textboxed (Entry controls) and assigned to corresponding variables, 
         # after converting them to numerical values; after the monthly payment is returned, it is assigned to a variable for output in the window and 
         #total payment is calculated and assigned to a variable to output in the window
-        monthlyPayment = self.getMonthlyPayment(
-            float(self.loanAmountVar.get()), 
-            float(self.annualInterestRateVar.get()) / 1200, 
-            int(self.numberOfYearsVar.get()))
-
-        self.monthlyPaymentVar.set(format(monthlyPayment, '10.2f'))
-        totalPayment = float(self.monthlyPaymentVar.get()) * 12 \
-            * int(self.numberOfYearsVar.get())
-        self.totalPaymentVar.set(format(totalPayment, '10.2f'))
-
+        TaxAndShipping = self.getTaxAndShipping(
+            int(self.Subtotal.get())
+        )
+        self.TaxAndShippingVar.set(format(TaxAndShipping, '10.2f'))
+        FinalPrice = float(self.TaxAndShippingVar.get()) + int(self.Subtotal.get())
+        self.FinalPrice.set(format(FinalPrice,'10.2f'))
+      
 
 OrderCostCalculator()  # Create GUI 
